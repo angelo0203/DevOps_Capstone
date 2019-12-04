33c9f78f8072a72928fdfb429d27d4e7bf24983e
angelo0203 <angelo0203@gmail.com>
2019-12-05 00:05:51 +0800

    adding eks

--- create_stack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack \
---stack-name $1 \
---region us-east-2 \
---template-body file://$2 \
---capabilities CAPABILITY_IAM
--- dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:1.17-alpine
-
-COPY app /usr/share/nginx/html/
-
